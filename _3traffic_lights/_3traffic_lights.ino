fb4a0d09e612f5c5adfed5387bb8bc06f1d49292
Kleii203 <160496670+FredAndrei203@users.noreply.github.com>
2025-10-11 21:45:00 +0800

    Rewriting

--- _3traffic_lights/_3traffic_lights.ino
+++ _3traffic_lights/_3traffic_lights.ino
@@ -1,133 +1,134 @@
-/*A project in which we created 2 traffic lights that are synchronized. Both have a button that works only when the light is red, and the 10-second counter is still below 6
-  seconds. When pushed, the counter is increased by 5 seconds. It can only be pressed once per instance of red light.*/
-
-enum LightState {
-  RED_LIGHT,
-  YELLOW_LIGHT,
-  GREEN_LIGHT
-};
-
-struct TrafficLight {
-  int red_pin, yellow_pin, green_pin, button_pin;
-  LightState state;
-  bool button_pushed;
-
-  void _turn_of_all_lights() {
-    digitalWrite(red_pin, LOW);
-    digitalWrite(yellow_pin, LOW);
-    digitalWrite(green_pin, LOW);
-  }
-
-  void light_up_green() {
-    state = GREEN_LIGHT;
-    _turn_of_all_lights();
-    digitalWrite(green_pin, HIGH);
-  }
-
-  void light_up_yellow() {
-    state = YELLOW_LIGHT;
-    _turn_of_all_lights();
-    digitalWrite(yellow_pin, HIGH);
-  }
-
-  void light_up_red() {
-    state = RED_LIGHT;
-    _turn_of_all_lights();
-    digitalWrite(red_pin, HIGH);
-  }
-};
-
-TrafficLight t_light1 = {4, 3, 2, 6, GREEN_LIGHT, false};
-TrafficLight t_light2 = {10, 9, 8, 5, RED_LIGHT, false};
-
-TrafficLight* currenty_red = &t_light2;
-
-long processed_timer = 1;
-long time_modifier = 0;
-long counter = 1;
-const long CYCLE_LENGTH = 10;
+//Synchronized traffic lights (timer = 10 seconds)(counts up).
+//If a traffic light's button is pushed when it's state is red, the timer increments by 5.
+const long TIMER_LENGTH = 12;
 const long OFFSET = 1;
+const long NON_YELLOW_STATE_START = 1;
+const long YELLOW_STATE_START = 10;
+
+long synchronized_counter_n = 1;
+long time_modifier = 0;
+long time_at_n_minus_1 = 1;
 
 bool activated = false;
 
-const long GREEN_RED_START = 1;
-const long YELLOW_START = 9;
+enum TrafficLightState {
+  RED_STATE,
+  YELLOW_STATE,
+  GREEN_STATE
+};
+
+struct TrafficLight {
+  int red_out, yellow_out, green_out, input_pin;
+  bool input_already_recieved;
+  TrafficLightState tl_state;
+
+  void deactivate_lights() {
+    digitalWrite(red_out, LOW);
+    digitalWrite(yellow_out, LOW);
+    digitalWrite(green_out, LOW);
+  }
+
+  void to_red_state() {
+    deactivate_lights();
+    digitalWrite(red_out, HIGH);
+    tl_state = RED_STATE;
+  }
+
+  void to_yellow_state() {
+    deactivate_lights();
+    digitalWrite(yellow_out, HIGH);
+    tl_state = YELLOW_STATE;
+  }
+
+  void to_green_state() {
+    deactivate_lights();
+    digitalWrite(green_out, HIGH);
+    tl_state = GREEN_STATE;
+  }
+};
+
+TrafficLight traffic_light1 = { 4, 3, 2, 6, false, GREEN_STATE };
+TrafficLight traffic_light2 = { 10, 9, 8, 5, false, RED_STATE };
+
+TrafficLight* currenty_red = &traffic_light2;
+
 
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
-  int output_pins[] = {t_light1.red_pin, t_light1.yellow_pin, t_light1.green_pin, t_light2.red_pin, t_light2.yellow_pin, t_light2.green_pin};
-  for(int idx = 0; idx < 6; idx++) {
+  int output_pins[] = { traffic_light1.red_out, traffic_light1.yellow_out, traffic_light1.green_out, traffic_light2.red_out, traffic_light2.yellow_out, traffic_light2.green_out };
+  for (int idx = 0; idx < 6; idx++) {
     pinMode(output_pins[idx], OUTPUT);
   }
-  pinMode(t_light1.button_pin, INPUT);
-  pinMode(t_light2.button_pin, INPUT);
-  initialize_states();
+  pinMode(traffic_light1.input_pin, INPUT);
+  pinMode(traffic_light2.input_pin, INPUT);
+  set_initial_states_of_traffic_lights();
   activated = true;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(activated) run_timer();
-  if(activated && time_has_advanced()) {
-    Serial.println(processed_timer);
-    check_state_switching_of(t_light1);
-    check_state_switching_of(t_light2);
+  if (activated)
+    continue_timer();
+  if (activated && has_time_changed()) {
+    Serial.println(synchronized_counter_n);
+    manage_state_machine_of(traffic_light1);
+    manage_state_machine_of(traffic_light2);
   }
-  check_input();
+  read_input();
 }
 
-void initialize_states() {
-  t_light1.light_up_green();
-  t_light2.light_up_red();
+void set_initial_states_of_traffic_lights() {
+  traffic_light1.to_green_state();
+  traffic_light2.to_red_state();
 }
 
-void run_timer() {
-  processed_timer = ((millis() / 1000) + time_modifier) % CYCLE_LENGTH + OFFSET;
+void continue_timer() {
+  synchronized_counter_n = ((millis() / 1000) + time_modifier) % TIMER_LENGTH + OFFSET;
 }
 
-bool time_has_advanced() {
-  bool needs_to = false;
-  if(processed_timer != counter) {
-    counter = processed_timer;
-    needs_to = true;
+bool has_time_changed() {
+  bool it_has = false;
+  if (synchronized_counter_n != time_at_n_minus_1) {
+    time_at_n_minus_1 = synchronized_counter_n;
+    it_has = true;
   }
-  return needs_to;
+  return it_has;
 }
 
-void check_state_switching_of(TrafficLight& traffic_light) {
-    switch(traffic_light.state) {
-    case RED_LIGHT:
-      if(processed_timer == GREEN_RED_START) {
-        traffic_light.light_up_green();
-        traffic_light.button_pushed = false;
+void manage_state_machine_of(TrafficLight& traffic_light) {
+  switch (traffic_light.tl_state) {
+    case RED_STATE:
+      if (synchronized_counter_n == NON_YELLOW_STATE_START) {
+        traffic_light.to_green_state();
+        traffic_light.input_already_recieved = false;
       }
       break;
-    case GREEN_LIGHT:
-      if(processed_timer >= YELLOW_START) {
-        traffic_light.light_up_yellow();
+    case GREEN_STATE:
+      if (synchronized_counter_n >= YELLOW_STATE_START) {
+        traffic_light.to_yellow_state();
       }
       break;
-    case YELLOW_LIGHT:
-      if(processed_timer == GREEN_RED_START) {
-        traffic_light.light_up_red();
+    case YELLOW_STATE:
+      if (synchronized_counter_n == NON_YELLOW_STATE_START) {
+        traffic_light.to_red_state();
         currenty_red = &traffic_light;
       }
       break;
   }
 }
 
-void check_input() {
-  if (digitalRead(currenty_red -> button_pin) == HIGH) {
-    button_pushed(currenty_red -> button_pushed);
+void read_input() {
+  if (digitalRead(currenty_red->input_pin) == HIGH) {
+    process_input(currenty_red->input_already_recieved);
   }
 }
 
 
-void button_pushed(bool& button_pushed) {
-  if(button_pushed == true || processed_timer > 5) {
+void process_input(bool& input_already_recieved) {
+  if (input_already_recieved == true || synchronized_counter_n > 5) {
     return;
   }
   time_modifier += 5;
-  button_pushed = true;
+  input_already_recieved = true;
 }
